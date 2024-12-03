bbfd968a2e34290db71a68ed385e8b99c05867a8
LBBassani <lorena.bassani@motora.ai>
2024-12-03 12:14:53 -0300

    fix: main com resto de código de teste

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,5 @@ async function bootstrap() {
   app.enableCors();
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
-
-  const driverService = app.get('DriversService');
-  driverService.postDriver({ name: 'John Doe', age: 30 });
 }
 bootstrap();
